33ce099f51b2ddb74b9f753d6991f3caef203b79
Simon Loir <simon-loir@hotmail.com>
2022-10-15 13:36:50 +0200

    user agent in electron

--- apps/agent/src/main.ts
+++ apps/agent/src/main.ts
@@ -1,2 +1,37 @@
 import * as Leap from 'leapjs';
-console.log('Agent started');
+import { app, BrowserWindow } from 'electron';
+import serve from 'electron-serve';
+import * as path from 'path';
+
+const dev = process.env.NODE_ENV === 'development';
+const loadURL = serve({ directory: 'app' });
+const appUrl = dev ? 'http://localhost:3000' : 'app://./index.html';
+
+console.log('dev mode : ', dev, appUrl);
+
+async function newWindow() {
+    const main = new BrowserWindow({
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            sandbox: true,
+        },
+        title: 'User Agent',
+    });
+
+    main.setMenu(null);
+
+    if (!dev) await loadURL(main);
+    main.loadURL(appUrl);
+}
+
+app.whenReady().then(async () => {
+    // Used on macOS device when the app is in the dock but no windows are open.
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) newWindow();
+    });
+});
+
+app.on('window-all-closed', function () {
+    // Prevents the app from quitting on macOS devices
+    if (process.platform !== 'darwin') app.quit();
+});
--- apps/main/src/main.ts
+++ apps/main/src/main.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World 2!');
 });
 
 app.listen(3001, () => {
