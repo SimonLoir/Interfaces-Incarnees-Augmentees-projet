ef527797fa07b474bc6e9f97f96d4046052077c9
Simon Loir <simon-loir@hotmail.com>
2022-10-15 15:59:09 +0200

    added some eslint rules

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -4,5 +4,10 @@ module.exports = {
         '@next/next/no-html-link-for-pages': 'off',
         'react/jsx-key': 'off',
         indent: ['error', 4],
+        'no-var': 'error',
+        'prefer-const': 'warn',
+        'no-unused-vars': 'error',
+        'func-call-spacing': ['error', 'never'],
+        'key-spacing': [2, { beforeColon: false, afterColon: true }],
     },
 };
