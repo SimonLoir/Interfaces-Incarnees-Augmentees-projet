ee4fbc1369db3edf475a2fe03a31645a9b262e89
Simon Loir <simon-loir@hotmail.com>
2022-10-16 14:44:54 +0200

    working web socket connection

--- apps/main/pages/_app.tsx
+++ apps/main/pages/_app.tsx
@@ -1,5 +1,9 @@
 import { AppProps } from 'next/app';
 import '../style/index.scss';
+import { useSocket } from '../utils/sockets';
 export default function MyApp({ Component, pageProps }: AppProps) {
+    const { connected, socket } = useSocket();
+    if (!connected) return <div>Connecting...</div>;
+    console.log(socket);
     return <Component {...pageProps} />;
 }
--- apps/main/src/main.ts
+++ apps/main/src/main.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import * as dotenv from 'dotenv';
 import { existsSync } from 'fs';
 import next from 'next';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
 if (existsSync('.env.local')) dotenv.config({ path: '.env.local' });
 
 const dev = process.env.NODE_ENV !== 'production';
@@ -13,11 +15,22 @@ const port = process.env.PORT || 3001;
     try {
         await app.prepare();
         const server = express();
+        const httpServer = createServer(server);
+        const io = new Server(httpServer);
 
-        server.all('*', (req: Request, res: Response) => {
+        io.on('connection', () => {
+            console.log('a user connected');
+        });
+
+        server.all('/', (req: Request, res: Response) => {
             return handle(req, res);
         });
-        server.listen(port, (err?: any) => {
+
+        server.all('/_next/*', (req: Request, res: Response) => {
+            return handle(req, res);
+        });
+
+        httpServer.listen(port, (err?: any) => {
             if (err) throw err;
             console.log(
                 `> Ready on localhost:${port} - env ${process.env.NODE_ENV}`
