28accca8f627217c0c00da11750823460e43b7be
Simon Loir <simon-loir@hotmail.com>
2022-10-20 14:45:46 +0200

    moved kinect code to the kinect class

--- apps/server/src/KinectServer.ts
+++ apps/server/src/KinectServer.ts
@@ -6,3 +6,35 @@ export default class KinectServer {
         console.log(kinect, server);
     }
 }
+
+//const Kinect2 = require('kinect2');
+//const kinect = new Kinect2();
+
+/*
+        const startKinect = () => {
+            if (kinect.open()) {
+                kinect.openBodyReader();
+                kinect.on('bodyFrame', (bodyFrame: any) => {
+                    drawBodyFrame(bodyFrame);
+                });
+            }
+        };
+
+        startKinect();
+
+let drawBodyFrame = (bodyFrame: any) => {
+    let bodyIndex = 0;
+    bodyFrame.array.forEach((body: any) => {
+        console.log('Body nb ' + bodyIndex);
+        if (body.tracked) {
+            for (const jointType in body.joints) {
+                const joint = body.joints[jointType];
+                if (joint.trackingState > Kinect2.TrackingState.notTracked) {
+                    console.log('hi2');
+                }
+            }
+        }
+    });
+};
+
+*/
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -7,34 +7,3 @@ if (existsSync('.env.local')) dotenv.config({ path: '.env.local' });
     const server = await Server.getInstance().start();
     console.log(server);
 })();
-//const Kinect2 = require('kinect2');
-//const kinect = new Kinect2();
-
-/*
-        const startKinect = () => {
-            if (kinect.open()) {
-                kinect.openBodyReader();
-                kinect.on('bodyFrame', (bodyFrame: any) => {
-                    drawBodyFrame(bodyFrame);
-                });
-            }
-        };
-
-        startKinect();
-
-let drawBodyFrame = (bodyFrame: any) => {
-    let bodyIndex = 0;
-    bodyFrame.array.forEach((body: any) => {
-        console.log('Body nb ' + bodyIndex);
-        if (body.tracked) {
-            for (const jointType in body.joints) {
-                const joint = body.joints[jointType];
-                if (joint.trackingState > Kinect2.TrackingState.notTracked) {
-                    console.log('hi2');
-                }
-            }
-        }
-    });
-};
-
-*/
