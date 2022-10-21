54696943cefb96998635076b555b709cf5f8c6c1
Simon Loir <simon-loir@hotmail.com>
2022-10-21 15:22:32 +0200

    removed electron agent

--- apps/agent-gui/pages/index.tsx
+++ apps/agent-gui/pages/index.tsx
@@ -1,33 +1,3 @@
-import { useEffect, useState } from 'react';
-import DisplayChooser from '../components/displays';
-
-export default function Web() {
-    const [sources, setSources] = useState<VideoSource[]>([]);
-    useEffect(() => {
-        const cb = ({ data }: MessageEvent) => {
-            if (data.type === 'sources') {
-                const { sources }: { sources: VideoSource[] } = data;
-                setSources(
-                    sources.filter(
-                        (s) => s.thumbnail !== 'data:image/png;base64,'
-                    )
-                );
-            }
-        };
-        window.addEventListener('message', cb);
-        return () => window.removeEventListener('message', cb);
-    }, []);
-    return (
-        <div>
-            <h1>IronProf Agent</h1>
-            <button
-                onClick={() => {
-                    window.postMessage('get-sources');
-                }}
-            >
-                Get Sources
-            </button>
-            <DisplayChooser sources={sources} />
-        </div>
-    );
+export default function Index() {
+    return <></>;
 }
--- apps/agent/README.md
+++
@@ -1,1 +0,0 @@
-# IronProf Agent
