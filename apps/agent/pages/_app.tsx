d462974ad9405f3f93a715be3ec0b664fb3a2370
Simon Loir <simon-loir@hotmail.com>
2022-10-21 16:09:29 +0200

    added socket io to the agent

--- apps/agent/pages/_app.tsx
+++ apps/agent/pages/_app.tsx
@@ -1,5 +1,13 @@
 import { AppProps } from 'next/app';
-import '@style/index.scss';
+import '../style/index.scss';
+import { useSocket, SocketContext } from '@utils/global';
+
 export default function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    const { connected, socket } = useSocket('http://localhost:3000');
+    if (!connected) return <div>Connecting...</div>;
+    return (
+        <SocketContext.Provider value={socket}>
+            <Component {...pageProps} />
+        </SocketContext.Provider>
+    );
 }
