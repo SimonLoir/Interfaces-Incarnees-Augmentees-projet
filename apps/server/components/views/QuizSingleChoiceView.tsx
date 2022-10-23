acb2637752616074b78647f1eb1072ead5b39e85
SteinsHeir <60748159+SteinsHeir@users.noreply.github.com>
2022-10-23 22:25:12 +0200

    adding poll states

--- apps/server/components/views/QuizSingleChoiceView.tsx
+++ apps/server/components/views/QuizSingleChoiceView.tsx
@@ -1,45 +1,105 @@
 import { ReactElement, useEffect, useState } from 'react';
 import style from '@style/QuizSingleChoice.module.scss';
 
+type stateType = 'creation' | 'awaiting' | 'ongoing';
+
 export default function QuizSingleChoice() {
     const [questionList, setQuestionList] = useState<Array<string>>([]);
+    const [currentState, setCurrentState] = useState<stateType>('creation');
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
-    return (
-        <div className={style.main}>
-            <form
-                name='questions'
-                onSubmit={(e) => {
-                    e.preventDefault();
-                    setQuestionList((list) => [
-                        ...list,
-                        document.forms.questions.elements.question_input.value,
-                    ]);
-                    console.log(questionList);
-                }}
-            >
-                <input
-                    required={true}
-                    name='question_input'
-                    type='text'
-                    placeholder='Poll name'
-                ></input>
-                <button type='submit'>add question</button>
-            </form>
-            <div>
-                {questionList.length > 0 && (
+    if (currentState === 'awaiting') {
+        return (
+            <div className={style.main}>
+                <p>Waiting for people to connect</p>
+                <div>
                     <div>
                         {questionList.map((question) => {
                             return <p>{question}</p>;
                         })}
                     </div>
-                )}
-
-                {questionList.length !== 0 && (
-                    <button type='button' onClick={() => {}}>
-                        Start quizz
+                    <button onClick={() => setCurrentState('ongoing')}>
+                        start poll
                     </button>
-                )}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+
+    if (currentState === 'ongoing') {
+        return (
+            <div className={style.mainOngoing}>
+                <div className={style.question}>
+                    <p>{questionList[currentQuestionIndex]}</p>
+                </div>
+                <div className={style.menu}>
+                    {!(currentQuestionIndex >= questionList.length) && (
+                        <div>
+                            <div>
+                                <table>
+                                    <tr>
+                                        <th>answers : </th>
+                                        <th>right</th>
+                                        <th>wrong</th>
+                                    </tr>
+                                </table>
+                            </div>
+                            <button
+                                onClick={() =>
+                                    setCurrentQuestionIndex((i) => i + 1)
+                                }
+                            >
+                                next
+                            </button>
+                        </div>
+                    )}
+                </div>
+            </div>
+        );
+    }
+
+    if (currentState === 'creation') {
+        return (
+            <div className={style.main}>
+                <form
+                    name='questions'
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        setQuestionList((list) => [
+                            ...list,
+                            document.forms.questions.elements.question_input
+                                .value,
+                        ]);
+                        console.log(questionList);
+                    }}
+                >
+                    <input
+                        required={true}
+                        name='question_input'
+                        type='text'
+                        placeholder='Poll name'
+                    ></input>
+                    <button type='submit'>add question</button>
+                </form>
+                <div>
+                    {questionList.length > 0 && (
+                        <div>
+                            {questionList.map((question) => {
+                                return <p>{question}</p>;
+                            })}
+                        </div>
+                    )}
+
+                    {questionList.length !== 0 && (
+                        <button
+                            type='button'
+                            onClick={() => setCurrentState('awaiting')}
+                        >
+                            Start poll
+                        </button>
+                    )}
+                </div>
+            </div>
+        );
+    }
 }
