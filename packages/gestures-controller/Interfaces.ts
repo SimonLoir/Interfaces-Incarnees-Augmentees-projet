eb7e160cca93343014ba5752066b1b51e5bbae2c
Simon LOIR <simon.loir@student.unamur.be>
2022-10-30 23:57:09 +0100

    gesture description interface

--- packages/gestures-controller/Interfaces.ts
+++ packages/gestures-controller/Interfaces.ts
@@ -1,5 +1,16 @@
-export interface Gesture {
+export interface Gesture<T extends 'static' | 'dynamic'> {
     name: string;
-    type: 'static' | 'dynamic';
+    type: T;
     description: string;
+    data: T extends 'static' ? Features : Features[];
+}
+
+export interface Features {
+    hands?: HandFeatures[];
+    extendedFingersCount?: number;
+    handsCount?: number;
+}
+
+export interface HandFeatures {
+    type?: 'left' | 'right';
 }
