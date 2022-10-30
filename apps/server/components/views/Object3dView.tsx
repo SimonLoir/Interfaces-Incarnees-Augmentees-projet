aa5b3145e71b0d1440db575f3ac893a0a989c020
Simon Loir <simon-loir@hotmail.com>
2022-10-30 14:29:28 +0100

    3D objects

--- apps/server/components/views/Object3dView.tsx
+++ apps/server/components/views/Object3dView.tsx
@@ -1,3 +1,37 @@
-export default function Object3dView() {
-    return <></>;
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { useLoader } from '@react-three/fiber';
+import { useRef } from 'react';
+
+function Scene() {
+    const obj = useLoader(OBJLoader, '/3d-model.obj');
+    const ref = useRef<THREE.Mesh>(null);
+
+    useFrame(() => {
+        ref.current?.rotateY(0.01);
+    });
+
+    return (
+        <mesh scale={0.05} position={[0, 0, -2]} ref={ref}>
+            <primitive object={obj} />
+            <meshStandardMaterial color={'orange'} />
+        </mesh>
+    );
+}
+
+export default function Object3DView() {
+    return (
+        <>
+            <Canvas
+                style={{
+                    height: '100%',
+                    width: '100%',
+                    background: 'rgb(34, 34, 34)',
+                }}
+            >
+                <ambientLight />
+                <Scene />
+            </Canvas>
+        </>
+    );
 }
--- apps/server/pages/3d.tsx
+++ apps/server/pages/3d.tsx
@@ -1,37 +1,5 @@
-import { Canvas, useFrame } from '@react-three/fiber';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
-import { useLoader } from '@react-three/fiber';
-import { useRef } from 'react';
+import Object3DView from 'components/views/Object3dView';
 
-function Scene() {
-    const obj = useLoader(OBJLoader, '/3d-model.obj');
-    const ref = useRef<THREE.Mesh>(null);
-
-    useFrame(() => {
-        ref.current?.rotateY(0.01);
-    });
-
-    return (
-        <mesh scale={0.05} position={[0, 0, -2]} ref={ref}>
-            <primitive object={obj} />
-            <meshStandardMaterial color={'orange'} />
-        </mesh>
-    );
-}
-
-export default function App3DPage() {
-    return (
-        <>
-            <Canvas
-                style={{
-                    height: '100vh',
-                    width: '100vw',
-                    background: 'rgb(34, 34, 34)',
-                }}
-            >
-                <ambientLight />
-                <Scene />
-            </Canvas>
-        </>
-    );
+export default function Object3D() {
+    return <Object3DView />;
 }
