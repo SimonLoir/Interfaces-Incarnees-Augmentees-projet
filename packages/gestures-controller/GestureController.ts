3985f16339441cbb31a7a47e44a505a6e6b0e093
Simon LOIR <simon.loir@student.unamur.be>
2022-11-01 00:19:50 +0100

    added comments

--- packages/gestures-controller/GestureController.ts
+++ packages/gestures-controller/GestureController.ts
@@ -1,6 +1,11 @@
 import Leap from 'leapjs';
-import { Model, Gesture, HandModel, FrameDiffExport } from './Interfaces';
-import { getAt } from '@utils/global';
+import {
+    Model,
+    Gesture,
+    HandModel,
+    FrameDiffExport,
+    HandDiffExport,
+} from './Interfaces';
 import {
     oneFingerUpGesture,
     twoFingersUpGesture,
@@ -9,8 +14,6 @@ import {
     fiveFingersUpGesture,
 } from './gestures/hand-counting';
 import FrameDiff from './FrameDiff';
-
-console.log(oneFingerUpGesture);
 
 export default abstract class GesturesController {
     protected staticGestures: Gesture<'static'>[] = [
@@ -28,9 +31,12 @@ export default abstract class GesturesController {
         allowedGestures: string[] = []
     ) {
         if (allowedGestures.length !== 0) {
+            // Filters the static gestures to keep only the allowed ones
             this.staticGestures = this.staticGestures.filter((gesture) =>
                 allowedGestures.includes(gesture.name)
             );
+
+            // Filters the dynamic gestures to keep only the allowed ones
             this.dynamicGestures = this.dynamicGestures.filter((gesture) =>
                 allowedGestures.includes(gesture.name)
             );
@@ -41,8 +47,15 @@ export default abstract class GesturesController {
         return new FrameDiff(from, to);
     }
 
+    /**
+     * Extracts a list of gestures from a buffer of frames
+     * @param frames A frame buffer
+     * @returns a list of gestures found in the buffer
+     */
     public extractFromFrames(frames: Leap.Frame[]): Gesture<any>[] {
+        // A list of gestures found in the buffer
         const gesturesFound: Gesture<any>[] = [];
+        // A list of gestures that are currently being checked
         const gestures: Gesture<any>[] = [
             ...this.staticGestures,
             ...this.dynamicGestures,
@@ -62,34 +75,50 @@ export default abstract class GesturesController {
 
         return gesturesFound;
     }
-
+    /**
+     * Checks if the frame buffer matches the model of the gesture
+     * @param gesture The gesture
+     * @param frames A buffer of frames
+     * @returns true if the buffer matches the model, false otherwise
+     */
     public matchStaticGesture(
         gesture: Gesture<'static'>,
         frames: Leap.Frame[]
     ): boolean {
+        // Gets the last frame in the buffer
         const lastFrame = frames[frames.length - 1];
+
+        // Checks that the last frame matches the model. If it does not, the gesture does not match
         if (!this.checkStaticPropertiesForModel(gesture.data, lastFrame))
             return false;
 
+        // Retrieves the minimum duration of the gesture from the model
         const { minDuration } = gesture.data;
+
         let i = -2;
-        let currentFrame = frames[frames.length + i];
+        let firstFrame = frames[frames.length + i];
+
+        // Gets the first frame that matches the model's duration
         while (
-            currentFrame &&
-            currentFrame.timestamp - lastFrame.timestamp < minDuration
+            firstFrame &&
+            firstFrame.timestamp - lastFrame.timestamp < minDuration
         ) {
             i--;
-            currentFrame = frames[frames.length + i];
+            firstFrame = frames[frames.length + i];
         }
 
+        // If there is no frame that matches the model's duration, the gesture does not match
+        // if the first frame that matches the model's duration does not match the model, the gesture does not match
         if (
-            !currentFrame ||
-            !this.checkStaticPropertiesForModel(gesture.data, currentFrame)
+            !firstFrame ||
+            !this.checkStaticPropertiesForModel(gesture.data, firstFrame)
         )
             return false;
 
-        const frameDiff = this.frameDiff(currentFrame, lastFrame);
+        // Gets the differences between the first frame and the last frame
+        const frameDiff = this.frameDiff(firstFrame, lastFrame);
 
+        // Checks if the differences between the first frame and the last frame match the model
         return this.checkDynamicPropertiesForModel(
             gesture.data,
             frameDiff.export()
@@ -184,7 +213,73 @@ export default abstract class GesturesController {
         return true;
     }
 
+    /**
+     * Checks if two frames match the model considering the motion
+     * @param model The model
+     * @param diff The difference between the two frames
+     * @returns true if the frames match the model, false otherwise
+     */
     public checkDynamicPropertiesForModel(model: Model, diff: FrameDiffExport) {
-        return false;
+        // Gets the hands in the model
+        const { hands: handsInModel } = model;
+        // Gets the hands difference in the frames
+        const {
+            handDiffs: handsInDiff,
+        }: { handDiffs: { [key: string]: HandDiffExport } } = diff;
+
+        if (handsInModel && handsInModel.length > 0) {
+            for (const handInModel of handsInModel) {
+                if (handInModel.type) {
+                    // Gets the hand of the type specified in the model from the frameDiff
+                    const handDiff = Object.values(handsInDiff).find(
+                        (h) => h.type === handInModel.type
+                    );
+                    // If the hand of the type specified in the model is not in the frameDiff, the gesture does not match
+                    if (!handDiff) return false;
+
+                    // Checks if the hand matches the model considering the motion
+                    if (!this.checkHandWithMotion(handInModel, handDiff))
+                        return false;
+                } else {
+                    // Checks if there is a hand in the frameDiff that matches the model considering the motion
+                    if (
+                        !Object.values(handsInDiff).some((h) =>
+                            this.checkHandWithMotion(handInModel, h)
+                        )
+                    )
+                        return false;
+                }
+            }
+        }
+
+        return true;
+    }
+    /**
+     * Checks if the hand matches the model considering the motion
+     * @param model The model of the hand
+     * @param diff The difference between two frames
+     * @returns true if the hand matches the model, false otherwise
+     */
+    public checkHandWithMotion(model: HandModel, diff: HandDiffExport) {
+        // Gets the expected velocity values from the model
+        const { palmVelocity } = model;
+
+        if (palmVelocity) {
+            // Gets the expected max and min values of velocity from the model
+            const { maxX, maxY, maxZ, minX, minY, minZ } = palmVelocity;
+            // Gets the actual velocity values from the diff
+            const {
+                velocityDiff: [velocityX, velocityY, velocityZ],
+            } = diff;
+
+            // Checks if the velocity values are in the expected range
+            if (maxX !== undefined && velocityX > maxX) return false;
+            if (maxY !== undefined && velocityY > maxY) return false;
+            if (maxZ !== undefined && velocityZ > maxZ) return false;
+            if (minX !== undefined && velocityX < minX) return false;
+            if (minY !== undefined && velocityY < minY) return false;
+            if (minZ !== undefined && velocityZ < minZ) return false;
+        }
+        return true;
     }
 }
