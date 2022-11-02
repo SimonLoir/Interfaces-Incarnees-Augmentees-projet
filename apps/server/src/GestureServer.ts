53f4edb11eefac72a27d04b5ab0ca59929e87837
Simon LOIR <simon.loir@student.unamur.be>
2022-11-02 17:50:57 +0100

    fixed issue with gestures

--- apps/server/src/GestureServer.ts
+++ apps/server/src/GestureServer.ts
@@ -3,7 +3,11 @@ import GestureController from 'gestures-controller';
 
 export default class GestureServer extends GestureController {
     constructor(private server: Server) {
-        super({}, ['thumbs-down', 'thumbs-up', 'screen-sharing']);
+        super({}, [
+            'thumb-position-down',
+            'thumb-position-up',
+            'screen-sharing',
+        ]);
         this.addEventListener('gesture', (g) => {
             console.log('received gesture', g);
             server.sendGesture(g);
