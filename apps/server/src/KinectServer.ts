7c09cd1c85fc2ae6bb3b4c1e64f93eb90c0455b4
Steins Heir <60748159+SteinsHeir@users.noreply.github.com>
2022-11-11 12:11:35 +0100

    adding small buffer and types for kinect frames analysis

--- apps/server/src/KinectServer.ts
+++ apps/server/src/KinectServer.ts
@@ -2,12 +2,40 @@ import Kinect2 from 'kinect2';
 import Server from './Server';
 import * as fs from 'fs';
 
+type Joint = {
+    depthX: number;
+    depthY: number;
+    colorX: number;
+    colorY: number;
+    cameraX: number;
+    cameraY: number;
+    cameraZ: number;
+    orientationX: number;
+    orientationY: number;
+    orientationZ: number;
+    orientationW: number;
+    jointType: number;
+    trackingState: number;
+};
+
+type KinectBody = {
+    bodyIndex: number;
+    tracked: boolean;
+    trackingId: number;
+    leftHandState: number;
+    rightHandState: number;
+    joints: Joint[];
+};
+
 export default class KinectServer {
+    private frameBuffer: KinectBody[];
+
     constructor(private kinect: Kinect2, private server: Server) {
         // The Kinect2 server can use the server's socket connection to send data to the client
         // exemple: this.server.sendNextView() and this.server.sendPreviousView()
         kinect;
         server;
+        this.frameBuffer = [];
     }
 
     startKinect = () => {
@@ -16,11 +44,32 @@ export default class KinectServer {
             this.kinect.on('bodyFrame', (bodyFrame: any) => {
                 bodyFrame.bodies.forEach((body: any) => {
                     if (body.tracked) {
-                        fs.writeFileSync(
-                            '../tests/kinectBodyFrame.json',
-                            JSON.stringify(body)
-                        );
-                        this.kinect.close();
+                        /*if (
+                            body.joints[4].trackingState !== 0 &&
+                            body.joints[5].trackingState !== 0
+                        )
+                            console.log(
+                                'left arm x : ' +
+                                    body.joints[4].cameraX
+                            );
+                        if (
+                            body.joints[8].trackingState !== 0 &&
+                            body.joints[9].trackingState !== 0
+                        )
+                            console.log(
+                                'right arm x : ' +
+                                    body.joints[8].cameraX
+                            );*/
+                        if (this.frameBuffer.length > 29) {
+                            this.frameBuffer.reverse().pop;
+                            this.frameBuffer.reverse;
+                            fs.writeFileSync(
+                                '../tests/kinectBodyFrame.json',
+                                JSON.stringify(this.frameBuffer)
+                            );
+                            this.kinect.close();
+                        }
+                        this.frameBuffer.push(body);
                     }
                 });
             });
