b9226deaa8ccad7dd0aefd3c31f560355ff16e09
Simon Loir <simon-loir@hotmail.com>
2022-11-20 12:21:05 +0100

    bodies store

--- packages/kinect-gestures-controller/Frame.ts
+++ packages/kinect-gestures-controller/Frame.ts
@@ -1,9 +1,9 @@
 import { BodyFrame } from 'kinect2';
 import { Body, joints as JoinTypes } from './types';
 export default class Frame {
-    private __bodies: Body[] = [];
-    constructor(initialFrame: BodyFrame) {
-        this.__bodies = initialFrame.bodies.map((body) => {
+    private __bodies: { [key: number]: Body } = {};
+    constructor(frame: BodyFrame) {
+        frame.bodies.forEach((body) => {
             const newBody: Body = {} as Body;
 
             if (!body.tracked) return newBody;
@@ -12,7 +12,8 @@ export default class Frame {
                 const jointType = JoinTypes[joint.jointType];
                 newBody[jointType] = joint;
             }
-            return newBody;
+
+            this.__bodies[body.trackingId] = newBody;
         });
     }
 }
