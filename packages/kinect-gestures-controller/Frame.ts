f7d9a6ab39fa6ac4305e3a1b16cbfcabb5bf47cd
Simon Loir <simon-loir@hotmail.com>
2022-11-20 12:33:32 +0100

    added frame ID and timestamp

--- packages/kinect-gestures-controller/Frame.ts
+++ packages/kinect-gestures-controller/Frame.ts
@@ -1,7 +1,11 @@
 import { BodyFrame } from 'kinect2';
 import { Body, joints as JoinTypes } from './types';
 export default class Frame {
+    private static id = 0;
     private __bodies: { [key: number]: Body } = {};
+    private __timestamp: number = Date.now();
+    private __id = Frame.id++;
+
     constructor(frame: BodyFrame) {
         frame.bodies.forEach((body) => {
             const newBody: Body = {} as Body;
@@ -15,5 +19,6 @@ export default class Frame {
 
             this.__bodies[body.trackingId] = newBody;
         });
+        console.log(this.__id, this.__timestamp);
     }
 }
