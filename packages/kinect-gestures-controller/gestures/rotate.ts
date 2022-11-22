a519ad2ed95200391b19236329abbea4c0358d8a
JohanRochet <91260794+JohanRochet@users.noreply.github.com>
2022-11-22 12:53:18 +0100

    rotate gesture

--- packages/kinect-gestures-controller/gestures/rotate.ts
+++ packages/kinect-gestures-controller/gestures/rotate.ts
@@ -1,64 +1,89 @@
 import { VectorModel } from 'project-types';
 import { Gesture } from '../types';
 
-const initFrameDirection: VectorModel = {
-    minX: -0.3,
-    maxX: 0.3,
-    minY: 0.5,
+const rightFrameDirection: VectorModel = {
+    minX: 0.5,
+    minY: -0.3,
+    maxY: 0.3,
 };
-const finalFrameDirection: VectorModel = {
-    minX: -0.3,
-    maxX: 0.3,
-    minY: 0,
+const leftFrameDirection: VectorModel = {
+    maxX: -0.5,
+    minY: -0.3,
     maxY: 0.3,
 };
 
-export const rotateGesture: Gesture<'dynamic'> = {
-    name: 'rotate',
-    type: 'dynamic',
-    description: 'Rotates the 3D Object',
-    coolDown: 1000,
-    data: [
-        {
-            minDuration: 0,
-            body: {
-                arms: [
-                    {
-                        direction: {
-                            ...initFrameDirection,
-                        },
-                    },
-                ],
-                forearms: [
-                    {
-                        direction: {
-                            ...initFrameDirection,
-                        },
-                    },
-                ],
-            },
-        },
-        {
-            minDuration: 0,
-            maxDuration: 1 * 1_000_000,
-            body: {
-                arms: [
-                    {
-                        direction: {
-                            ...finalFrameDirection,
-                        },
-                        velocityDiff: {},
-                    },
-                ],
-                forearms: [
-                    {
-                        direction: {
-                            ...finalFrameDirection,
-                        },
-                        velocityDiff: {},
-                    },
-                ],
-            },
-        },
-    ],
+const velocityDiffLeft: VectorModel = {
+    minX: -70,
+    minY: -30,
+    maxY: 30,
 };
+const velocityDiffRight: VectorModel = {
+    maxX: 70,
+    minY: -30,
+    maxY: 30,
+};
+
+function GenerateRotateGesture(type: 'left' | 'right'): Gesture<'dynamic'> {
+    return {
+        name: 'rotate-' + type,
+        type: 'dynamic',
+        description: 'Rotates the 3D Object to the ' + type,
+        coolDown: 1000,
+        data: [
+            {
+                minDuration: 0,
+                body: {
+                    arms: [
+                        {
+                            direction:
+                                type === 'right'
+                                    ? leftFrameDirection
+                                    : rightFrameDirection,
+                        },
+                    ],
+                    forearms: [
+                        {
+                            direction:
+                                type === 'right'
+                                    ? leftFrameDirection
+                                    : rightFrameDirection,
+                        },
+                    ],
+                },
+            },
+            {
+                minDuration: 0,
+                maxDuration: 1 * 1_000_000,
+                body: {
+                    arms: [
+                        {
+                            direction:
+                                type === 'right'
+                                    ? rightFrameDirection
+                                    : leftFrameDirection,
+                            velocityDiff:
+                                type === 'right'
+                                    ? velocityDiffRight
+                                    : velocityDiffLeft,
+                        },
+                    ],
+                    forearms: [
+                        {
+                            direction:
+                                type === 'right'
+                                    ? rightFrameDirection
+                                    : leftFrameDirection,
+                            velocityDiff:
+                                type === 'right'
+                                    ? velocityDiffRight
+                                    : velocityDiffLeft,
+                        },
+                    ],
+                },
+            },
+        ],
+    };
+}
+
+export const rotateLeftGesture = GenerateRotateGesture('left');
+export const rotateRightGesture = GenerateRotateGesture('right');
