c0db50cd8ed4fc0b67353185ab7ee51e21234326
JohanRochet <91260794+JohanRochet@users.noreply.github.com>
2022-11-24 23:53:21 +0100

    perfectly working swipe

--- packages/leap-gestures-controller/gestures/swipe.ts
+++ packages/leap-gestures-controller/gestures/swipe.ts
@@ -1,37 +1,5 @@
 import { Gesture, HandModel } from '../Interfaces';
 
-export const swipeLeftGesture: Gesture<'dynamic'> = {
-    name: 'swipe-left',
-    description: 'Swipe left',
-    type: 'dynamic',
-    coolDown: 1000,
-    data: [
-        {
-            minDuration: 0,
-            hands: [BackhandRight(), ForehandLeft()],
-            allowOnlyOneHandMatch: true,
-        },
-        {
-            minDuration: 0,
-            maxDuration: 1.5 * 1_000_000,
-            hands: [
-                {
-                    ...ForehandRight(),
-                    palmVelocity: {
-                        minX: 700,
-                    },
-                },
-                {
-                    ...BackhandLeft(),
-                    palmVelocity: {
-                        minX: 700,
-                    },
-                },
-            ],
-            allowOnlyOneHandMatch: true,
-        },
-    ],
-};
 export const swipeRightGesture: Gesture<'dynamic'> = {
     name: 'swipe-right',
     description: 'Swipe right',
@@ -40,7 +8,7 @@ export const swipeRightGesture: Gesture<'dynamic'> = {
     data: [
         {
             minDuration: 0,
-            hands: [ForehandRight(), BackhandLeft()],
+            hands: [RightHand(), LeftHand()],
             allowOnlyOneHandMatch: true,
         },
         {
@@ -49,13 +17,46 @@ export const swipeRightGesture: Gesture<'dynamic'> = {
             maxDuration: 1.5 * 1_000_000,
             hands: [
                 {
-                    ...BackhandRight(),
+                    ...RightHand(),
+                    palmVelocity: {
+                        minX: 700,
+                    },
+                },
+                {
+                    ...LeftHand(),
+                    palmVelocity: {
+                        minX: 700,
+                    },
+                },
+            ],
+            allowOnlyOneHandMatch: true,
+        },
+    ],
+};
+
+export const swipeLeftGesture: Gesture<'dynamic'> = {
+    name: 'swipe-left',
+    description: 'Swipe left',
+    type: 'dynamic',
+    coolDown: 1000,
+    data: [
+        {
+            minDuration: 0,
+            hands: [LeftHand(), RightHand()],
+            allowOnlyOneHandMatch: true,
+        },
+        {
+            minDuration: 0,
+            maxDuration: 1.5 * 1_000_000,
+            hands: [
+                {
+                    ...LeftHand(),
                     palmVelocity: {
                         maxX: -700,
                     },
                 },
                 {
-                    ...ForehandLeft(),
+                    ...RightHand(),
                     palmVelocity: {
                         maxX: -700,
                     },
@@ -66,66 +67,34 @@ export const swipeRightGesture: Gesture<'dynamic'> = {
     ],
 };
 
-function ForehandRight(): HandModel {
+function RightHand(): HandModel {
     return {
         type: 'right',
+
         fingers: {
             minExtended: 4,
         },
         palmNormal: {
-            maxX: 0,
+            maxX: -0.3,
             minX: -1,
-            maxY: 0.5,
-            minZ: -1,
-            maxZ: 0,
+            maxY: 0.7,
+            minY: -0.7,
         },
     };
 }
 
-function ForehandLeft(): HandModel {
+function LeftHand(): HandModel {
     return {
         type: 'left',
+
         fingers: {
             minExtended: 4,
         },
         palmNormal: {
             maxX: 1,
-            minX: 0,
-            maxY: 0.5,
-            minZ: -1,
-            maxZ: 0,
-        },
-    };
-}
-
-function BackhandRight(): HandModel {
-    return {
-        type: 'right',
-        fingers: {
-            minExtended: 4,
-        },
-        palmNormal: {
-            maxX: 0,
-            minX: -1,
-            minY: -0.5,
-            minZ: 0,
-            maxZ: 1,
-        },
-    };
-}
-
-function BackhandLeft(): HandModel {
-    return {
-        type: 'left',
-        fingers: {
-            minExtended: 4,
-        },
-        palmNormal: {
-            maxX: 1,
-            minX: 0,
-            minY: -0.5,
-            minZ: 0,
-            maxZ: 1,
+            minX: 0.3,
+            maxY: 0.7,
+            minY: -0.7,
         },
     };
 }
